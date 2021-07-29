0fc0e676d0f30c11dc5237f403c09f50b408edce
Giovanni Di Martino <Giovanni.DiMartino@nttdata.com>
2021-07-29 12:55:27 +0200

    removed unused import

--- src/pages/MainProtectedPage.js
+++ src/pages/MainProtectedPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { withRouter } from 'react-router-dom';
 import styled from "styled-components";
